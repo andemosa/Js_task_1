50ec9c0ce94f4139885d7d80b201adb9b25051e9
Anderson Osayerie <33299704+andemosa@users.noreply.github.com>
2020-03-25 23:58:13 +0100

    Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let name = "Anderson Osayerie"
+const courses = ["html", "css", "js"]
+
+console.log(name)
+console.log(courses)
+
+for (var x=0; x<=200; x++) {
+    if (x % 2 != 0) {
+            console.log(x);   
+    }
+}
